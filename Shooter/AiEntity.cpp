38718a38d96233e68c2bae46ccbe5856b74aed01
MathieuCaselles <mathieu.caselles@ynov.com>
2022-02-18 22:40:42 +0100

    add ai

--- Shooter/AiEntity.cpp
+++ Shooter/AiEntity.cpp
@@ -6,11 +6,15 @@ CAiEntity::CAiEntity(const char* name, sf::Sprite sprite):CEntity(name, sprite)
 	sf::Vector2f startPosition;
 	startPosition.y = (float)getRandomInt(80, 1000);
 	startPosition.x = 2000;
-	Sprite.setPosition(startPosition);
-	Sprite.setScale(0.5f, 0.5f);
 
-	Destination.x = (float)getRandomInt(960, 1900);
-	Destination.y = (float)getRandomInt(20, 1000);
+	const sf::FloatRect globalBounds = Sprite.getGlobalBounds();
+	Sprite.setOrigin(globalBounds.width / 2, globalBounds.height / 2);
+
+	Sprite.setPosition(startPosition);
+	Sprite.setScale(0.25f, 0.25f);
+
+
+	Destination = startPosition;
 }
 
 const sf::Vector2f& CAiEntity::GetDestination()
@@ -21,4 +25,13 @@ const sf::Vector2f& CAiEntity::GetDestination()
 void CAiEntity::SetDestination(sf::Vector2f newDestination)
 {
 	Destination = newDestination;
+}
+
+void CAiEntity::SetRandomDestination(sf::RenderWindow& Window)
+{
+	const sf::FloatRect globalBounds = Sprite.getGlobalBounds();
+	const sf::FloatRect viewport = (sf::FloatRect)Window.getViewport(Window.getView());
+
+	Destination.x = (float)getRandomInt((int)globalBounds.width, (int)(viewport.width - globalBounds.width));
+	Destination.y = (float)getRandomInt((int)globalBounds.height, (int)(viewport.height - globalBounds.height));
 }
